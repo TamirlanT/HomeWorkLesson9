15f2c30bf0ce06b53647a0d9ded78ee1d6597338
Tamirlan <tuarshintamir@gmail.com>
2022-06-03 10:42:46 +0500

    Сумма цифр в числе

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+Console.WriteLine("Введите число n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int DigitSum(int number)
+{
+    if (number == 0) return 0;
+    return number % 10 + DigitSum(number/10);
+    
+}
+Console.WriteLine(DigitSum(n));
